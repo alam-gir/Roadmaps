5411c3791bb3a01673b5811ce8d9e323bf3c43d5
alam-gir <info.alamgirhussain@gmail.com>
2025-06-15 12:44:37 +0600

    Initialize project with: - POSTGRESQL - JDBC

---
+++ src/main/java/com/roadmaps/Roadmaps/RoadmapsApplication.java
@@ -0,0 +1,13 @@
+package com.roadmaps.Roadmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadmapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roadmaps/Roadmaps/RoadmapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadmaps.Roadmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
