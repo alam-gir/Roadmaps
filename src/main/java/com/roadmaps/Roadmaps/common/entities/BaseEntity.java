75a25aac57fbb912ba73dabbc5ca26845bd10520
alam-gir <info.alamgirhussain@gmail.com>
2025-06-16 16:38:09 +0600

    Entity: - Created BaseEntity. It will extend all the entity. It has the common field. as for now just 'id'

---
+++ src/main/java/com/roadmaps/Roadmaps/common/entities/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.roadmaps.Roadmaps.common.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@SuperBuilder
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "UUID")
+    private UUID id;
+}
