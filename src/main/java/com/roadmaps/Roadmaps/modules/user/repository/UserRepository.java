5f49940798fe451ab24ca3ee072eb1a94ee7f767
alam-gir <info.alamgirhussain@gmail.com>
2025-06-16 18:22:55 +0600

    UserRepository : - annotate Repository in UserRepository

--- src/main/java/com/roadmaps/Roadmaps/modules/user/repository/UserRepository.java
+++ src/main/java/com/roadmaps/Roadmaps/modules/user/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.roadmaps.Roadmaps.modules.user.repository;
 
 import com.roadmaps.Roadmaps.modules.user.enities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmailIgnoreCase(String email);
 }
