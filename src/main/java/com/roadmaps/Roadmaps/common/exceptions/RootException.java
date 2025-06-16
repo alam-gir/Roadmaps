5f2b23deae4605e1e4c484476a4359f2a7175f5d
alam-gir <info.alamgirhussain@gmail.com>
2025-06-16 16:19:07 +0600

    Exceptions: - Created RootException   This will extend by all the exceptions. and then just call the super to generate a proper RuntimeException.

---
+++ src/main/java/com/roadmaps/Roadmaps/common/exceptions/RootException.java
@@ -0,0 +1,20 @@
+package com.roadmaps.Roadmaps.common.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class RootException extends RuntimeException{
+    public final String errorCode;
+    public final HttpStatus httpStatus;
+
+    public RootException (String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+        this.httpStatus = HttpStatus.BAD_REQUEST;
+    }
+
+    public RootException (String message, String errorCode, HttpStatus httpStatus) {
+        super(message);
+        this.errorCode = errorCode;
+        this.httpStatus = httpStatus;
+    }
+}
