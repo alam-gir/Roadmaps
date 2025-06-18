427231827b1c3cb3c0cc14bc2ddd0a5f7d5b3245
alam-gir <info.alamgirhussain@gmail.com>
2025-06-18 12:50:05 +0600

    Dependency update: - spring boot starter mail. to send mail. - spring retry. to retry execution task if failed.

--- src/main/java/com/roadmaps/Roadmaps/RoadmapsApplication.java
+++ src/main/java/com/roadmaps/Roadmaps/RoadmapsApplication.java
@@ -3,11 +3,13 @@ package com.roadmaps.Roadmaps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@EnableRetry
 public class RoadmapsApplication {
 
 	public static void main(String[] args) {
