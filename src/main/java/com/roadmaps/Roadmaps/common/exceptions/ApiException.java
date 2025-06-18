37df923566105a91c7ca9395f84875fd370fecb4
alam-gir <info.alamgirhussain@gmail.com>
2025-06-18 11:11:44 +0600

    Api exception updated with empty constructor for initial calling without message.

--- src/main/java/com/roadmaps/Roadmaps/common/exceptions/ApiException.java
+++ src/main/java/com/roadmaps/Roadmaps/common/exceptions/ApiException.java
@@ -6,4 +6,8 @@ public class ApiException extends RootException{
     public ApiException(String message) {
         super(message, HttpStatus.BAD_REQUEST.toString());
     }
+
+    public ApiException() {
+        super("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR.toString());
+    }
 }
